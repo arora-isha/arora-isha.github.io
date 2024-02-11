4871eca0151d64e1be4aa79c7fe69e30e416a0f5
arora-isha <159700058+arora-isha@users.noreply.github.com>
2024-02-11 18:30:54 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Isha Arora</title>
+	 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+   <link rel="stylesheet" type="text/css" href="css/style.css">
+      <link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+   <link rel="stylesheet" type="text/css" href="css/owl.theme.default.min.css">
+       <link rel="stylesheet" type="text/css" href="css/all.min.css">
+	       <link rel="stylesheet" type="text/css" href="css/flaticon.css">
+   </head>
+<body>
+
+<header>
+
+<section class="p70">
+<div class="container">
+<div class="row">
+	<div class="col-lg-12 text-center main-title">	
+<h1>Isha Arora</h1>
+<h5><a href="mailto:ia93@cornell.edu">ia93@cornell.edu</a> /<a href="https://www.linkedin.com/in/arora-isha/"> LinkedIn </a> / <a href="/IshaArora.pdf">CV</a></h5>
+<br>
+    </div>
+		<div class="col-lg-8">	
+		
+	<p>I’m a senior at Cornell University majoring in Biomedical Engineering and minoring in Computer Science. To explore my research interests at the intersection of computational biology and immunology, I have conducted research at the Jiang Lab at Cornell, the Azizi Lab at Columbia University, and the Berger Lab at the Massachusetts Institute of Technology. I have also completed a co-op at Moderna Therapeutics. From here on, I am interested in developing computational models, enabled by wet lab experimentation, to study disease progression and inform the development of novel affordable therapeutics.</p>
+	<p>Beyond this, I enjoy teaching and have been a TA for various courses. Currently, I am also a mentor for the Girls Who Code chapter at Cornell. I aim to motivate younger students to view science as an exciting field and enable them to pursue their interests in STEM.
+</p>	
+		<p>Outside of school, I love to dance, crochet, and explore nature.
+
+</p>
+	
+	 </div>
+	
+<div class="col-lg-4">	
+	<img src="images/isha.jpg" width="100%" alt="">
+	 </div>
+	 		<div class="col-lg-12">
+	 <h2>Research Experience:</h2>
+	<p><b>Jiang Lab</b><br> Here, I have conducted research alongside my PhD mentor to develop and test novel immunotherapies for cancer. My first project was to test the efficacy of a chemical compound as a training agent in cancer, whereas my second project involved the development a neoantigen-based therapeutic cancer vaccine for cold tumor models including pancreatic cancer (mechanism in image below). Now, I am pursuing an honors thesis wherein I am developing a therapeutic vaccine for a breast cancer model.
+</p> 
+
+<div class="row">
+		<div class="col-lg-12">
+	<img src="images/img-7.jpg" width="100%" alt="">
+<p>An mRNA-LNP construct is used to deliver neoantigen to dendritic cells. The neoantigen is expressed on the surface of the dendritic cell, activating T-cells against the tumor.
+</p>
+</div></div>
+
+
+	<p><b>Azizi Lab </b><br> Through the IICD Summer Research Program at Columbia University, I studied how copy number variation influences melanoma cancer response to anti-PD1 immunotherapy at the Azizi Lab. For this, I developed a probabilistic graphical machine learning model and a novel deconvolution algorithm.
+</p> 
+<br>
+<div class="row">
+		<div class="col-lg-3 text-center offset-md-4">
+	<img src="images/img-1.jpg" width="100%" alt="">
+<p><em>Poster Presentation at CUPP Poster Symposium 2022.</em></p>
+</div></div>
+
+	<p><b>Moderna Therapeutics </b><br> As a Bioinformatics co-op at Moderna, I developed an ML model to predict subcellular localization of proteins in bacteria, resulting in a first-author publication (under review at Computers in Biology and Medicine).
+</p> 
+
+Link :<a href="https://www.biorxiv.org/content/10.1101/2023.10.04.560913v1"> https://www.biorxiv.org/content/10.1101/2023.10.04.560913v1
+ </a>
+ <p></p>
+	<p><b>Berger Lab </b><br> To gain a deeper understanding of large language models, I pursued research at the Berger Lab. Here, I am using transformers to study synonymous and non-synonymous mutations from an evolutionary perspective. 
+</p> 
+	
+	 <h2>Teaching and Mentorship:</h2>
+<p>I’ve served as a teaching assistant at Cornell and developed teaching materials for discussion sections for the following BME and CS classes:
+</p>
+<p>1. Physiology of Human Health and Disease (Spring 2022) </p>
+<p>2. Object-Oriented Programming and Data Structures (Sprint 2021-Fall 2021)</p>
+<p>3. Introduction to programming in Python (Summer 2021)</p>
+<p>I have also mentored fellow undergraduate students at the Jiang and Berger labs. Beyond this, I believe that investing in a new generation of diverse and inclusive leaders is necessary to sustainably impact lives.</p>
+<p>To this end, I am actively involved in mentoring students through the Girls Who Code (GWC) program to help them learn the basics of coding and apply these skills towards a tangible project. GWC is solely comprised of female mentors, and thus, I excited to serve as a role model to younger girls in CS and show them that they can pursue anything that they set their minds to.
+</p>
+<p>I have also developed a mentorship program at the nonprofit United Way in Delhi, India. Moreover, I personally mentored three middle school students.
+
+</p>
+	
+		 <h2>Community Outreach</h2>
+	<p><b>Cornell Hunger Relief</b><br>I was involved in Cornell Hunger Relief (CHR) in my freshman year (November 2020 to May 2021) as Projects and Events Co-Chair and recently rejoined as Secretary this October. As a part of CHR, I have worked to organize and arrange various educational sessions including our Food Justice Seminar Series in Spring 2021 to spread awareness about food insecurity in Tompkins County amongst students and the Ithaca community. Recently I have also volunteered to pack meals for those in need. 
+</p>
+<div class="row">
+		<div class="col-lg-3 text-center offset-md-4">
+	<img src="images/img02.jpg" width="100%" alt="">
+<p><i>Volunteer Session for the FMSC Ithaca Mobile Pack</i></p>
+</div></div>
+ 	
+	
+		<p><h2>Personal</h2>:
+</b><br>I love dancing, swimming, exploring nature, crocheting, and embroidering.<br>
+I have been training in Bharatanatyam – a South Asian classical dance form – since a young age. I enjoy performing and choreographing and have done so as the co-captain and co-president of Anjali – a dance team at Cornell. 
+</p> 	
+<div class="row">
+		<div class="col-lg-6 text-center">
+	<img src="images/img-3.jpg" width="100%" alt="">
+<p><em>My Arangetram!</em></p>
+</div>
+		<div class="col-lg-6 text-center">
+	<img src="images/img-4.jpg" width="100%" alt="">
+<p><em>Performance at Diya Jale ‘23</em></p>
+</div>
+</div>
+
+
+
+
+<p>I also enjoy exploring gorges in Ithaca and going on hikes!</p>	
+<div class="row">
+		<div class="col-lg-3 text-center offset-md-4">
+	<img src="images/img-5.jpg" width="100%" alt="">
+<p><em>Hike in the Adirondacks, NY</em></p>
+</div>
+
+</div>
+<p>I love to embroider and recently picked up crocheting as well.
+</p>	
+	<div class="row">
+		<div class="col-lg-3 text-center offset-md-4">
+	<img src="images/img-6.jpg" width="100%" alt="">
+<p><em>Flowerpot that I recently crocheted</em></p>
+</div>
+
+</div>
+	
+	
+	
+	  </div>
+	 
+	 
+ 
+	</div>
+	</div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+   	 <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	
+
+	
+	
+	</body>
+</html>
